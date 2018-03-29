3b00fa4de01ba6f9ec73738ff5b43ff8b49ab903
Janno Järv <jannojarv@gmail.com>
2018-03-29 18:37:50 +0300

    Update environment.staging.ts

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   backendEndpoint: "https://api.elements.welton.ee",
   apiEndpoint: "https://api.elements.welton.ee/api",
-  transactionRouterEndpoint: "http://tx.elements.welton.ee",
+  transactionRouterEndpoint: "https://tx.elements.welton.ee",
   production: true
 };
