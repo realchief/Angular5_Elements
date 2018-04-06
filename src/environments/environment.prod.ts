f972b21ae07f8f3344e4d8cb043f3c351bd4482a
Janno Järv <jannojarv@gmail.com>
2018-04-06 16:46:27 +0300

    Updated transaction router URLs in environment configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   backendEndpoint: "https://elementsdax.com",
-  apiEndpoint: "https://api.elementsdax.com",
-  transactionRouterEndpoint: "https://tx.elementsdax.com",
+  apiEndpoint: "https://api.elementsdax.com/api",
+  transactionRouterEndpoint: "https://tx.elementsdax.com/hubs/transaction-router/",
   production: true
 };
