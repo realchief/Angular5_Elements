4a0308ec6556405174b2f1fa2d1d535f4271f9a7
Max Yevdokimov <max.windev@gmail.com>
2018-04-25 20:12:01 +0300

    Backoffice app: new login screen layout.

--- src/back-office-app/shared/auth-guard.service.ts
+++ src/back-office-app/shared/auth-guard.service.ts
@@ -6,26 +6,26 @@ import "rxjs/add/observable/of";
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanActivateChild {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean> | Promise <boolean> | boolean {
+  ): Observable<boolean> | Promise<boolean> | boolean {
     return this.check(state);
   }
 
   public canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable <boolean> | Promise <boolean> | boolean {
+  ): Observable<boolean> | Promise<boolean> | boolean {
     return this.check(state);
   }
 
   private check(state: RouterStateSnapshot) {
     const token = localStorage.getItem(TOKEN_STORAGE_KEY);
     if (!token) {
-      this.router.navigate(["/login"], { queryParams: { returnUrl: state.url } });
+      this.router.navigate(["/auth/login"], { queryParams: { returnUrl: state.url } });
     }
     return !!token;
   }
